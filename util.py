bd4b4c0edb66c18d154f91cda00bbd3aebeaf2c5
LazerCubegit config --global user.email git@lazercube.com <lazercube@users.noreply.github.com>
2015-12-14 19:51:25 +0000

    Updated format

--- util.py
+++ util.py
@@ -1,29 +1,31 @@
 import math
 import pygame
 
-#------DISPLAY INFO-----#
+# ------DISPLAY INFO-----#
 
-display_width = 500
-display_height = 500
+SURFACE_WIDTH = 500
+SURFACE_HEIGHT = 500
+SURFACE_CAPTION = "Python game engine"
 
+# --------COLOURS--------#
 
-#--------COLOURS--------#
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 40)
+LIGHT_GREY = (05, 05, 05)
+TERM_BLUE = (46, 158, 244)
+TERM_GRAY = (44, 44, 44)
+TERM_LIGHT_GRAY = (102, 102, 102)
+DARK_BLUE = (40, 44, 52)
 
-WHITE   = (255,255,255)
-BLACK   = (0,0,0)
-RED     = (255,0,0)
-GREEN   = (0,255,40)
-LIGHT_GREY = (05,05,05)
-TERM_BLUE = (46,158,244)
-TERM_GRAY = (44,44,44)
-TERM_LIGHT_GRAY = (102,102,102)
-DARK_BLUE = (40,44,52)
+DEFAULT_FONT = 'franklin gothic'
 
-default_font = 'franklin gothic'
 
 def cos(angle):
     x = math.radians(angle)
     return math.cos(x)
+
 
 def sin(angle):
     x = math.radians(angle)
