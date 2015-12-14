863a5ade9bc038577ac9c19d914c7fc073df001f
LazerCubegit config --global user.email git@lazercube.com <lazercube@users.noreply.github.com>
2015-12-14 19:50:57 +0000

    Simple Base for new engine

--- main.py
+++ main.py
@@ -1,7 +1,71 @@
 #!/usr/bin/python
-
 import pygame
+import world
+import util
 
+
+class _Game():
+    def __init__(self, SURFACE):
+        self.SURFACE = SURFACE
+        self.world = world.World(SURFACE)
+        self.width = self.world.WIDTH
+        self.height = self.world.HEIGHT
+        self._Run()
+
+    def _Draw_Hud(self):
+        pass
+
+    def _Draw_Info(self):
+        pass
+
+    def _Run(self):
+        _MS_PER_TICK = 15.625
+        _previous_time = pygame.time.get_ticks()
+        _previous_timer = pygame.time.get_ticks()
+        _current_time = 0
+
+        frames = 0
+        ticks = 0
+        delta = 0
+
+        while not(self.world.quit):
+            _current_time = pygame.time.get_ticks()
+            delta += ((_current_time - _previous_time) / _MS_PER_TICK)
+            _previous_time = _current_time
+
+            while(delta >= 1):
+                self.world.Update()
+                ticks += 1
+                delta -= 1
+
+            self._Render()
+            frames += 1
+
+            if(pygame.time.get_ticks() - _previous_timer >= 1000):
+                print("FPS: %i  TICKS: %i" % (frames, ticks))
+                _previous_timer = pygame.time.get_ticks()
+                frames = 0
+                ticks = 0
+
+    def _Render(self):
+        self.SURFACE.fill(util.BLACK)
+        self.world.draw()
+        self._Draw_Hud()
+        self._Draw_Info()
+
+        pygame.display.flip()
+
+
+def _Initiate():
+    pygame.init()
+    SURFACE = pygame.display.set_mode(
+        [util.SURFACE_WIDTH, util.SURFACE_HEIGHT], pygame.HWSURFACE |
+        pygame.DOUBLEBUF)
+    pygame.display.set_caption(util.SURFACE_CAPTION)
+
+    game = _Game(SURFACE)
+
+    pygame.quit()
 
 if __name__ == "__main__":
-    main()
+    _Initiate()
