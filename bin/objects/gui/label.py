98c0027b9d10b7e7a99607e9a611e79bb4cdf42f
LazerCube <git@lazercube.com>
2016-03-25 17:31:55 +0000

    Added some small improvments

---
+++ bin/objects/gui/label.py
@@ -0,0 +1,11 @@
+import pygame
+
+from utilites import util
+from objects import objects
+
+class Label(objects.GUI):
+    def __init__(self, world, text, fontsize, color, position):
+        super(Label, self).__init__(world, text, fontsize, color, position)
+
+    def Update(self):
+        super(Label, self).Update()
