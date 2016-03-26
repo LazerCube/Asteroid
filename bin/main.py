88ae6532f805c441ab9818fd3fa9cebdb484cc53
LazerCube <git@lazercube.com>
2016-03-26 14:34:24 +0000

    Added basic command line args. [-d] for debug mode.

--- bin/main.py
+++ bin/main.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python
-import world
-
+import sys
+import getopt
 import pygame
+
+import world
 import settings
 from utilites import util
 
 class _Game():
-    def __init__(self, SURFACE):
+    def __init__(self, argv, SURFACE):
         self.SURFACE = SURFACE
         self.WIDTH = SURFACE.get_width()
         self.HEIGHT = SURFACE.get_height()
         self.DEBUG_MODE = settings.DEBUG_MODE
         self.DEBUG_INFO = "None"
-        self.GAMESTATE = util.enum(world.MenuState(self),
-                                   world.GameState(self))
-
-        self.state = self.GAMESTATE.reverse_mapping[0]
-        self._state = None
 
         self.EXIT = False
 
@@ -24,7 +21,29 @@ class _Game():
         self.NUM = None
         self.HOST = None
 
+        self.commandLineArgs(argv)
+
+        self.GAMESTATE = util.enum(world.MenuState(self),
+                                   world.GameState(self))
+
+        self.state = self.GAMESTATE.reverse_mapping[0]
+        self._state = None
+
         self._Run()
+
+    def commandLineArgs(self, argv):
+       try:
+           opts, args = getopt.getopt(argv,"hd")
+       except getopt.GetoptError:
+           print '\nUsage main.py [-h] [-d]'
+           sys.exit(2)
+       for opt, arg in opts:
+           if opt == '-h':
+               print '\nOptional arguments: \n\n -h, --help  Shows this help message and exit\n -d, --debug  Runs program in debug mode.'
+               sys.exit()
+           elif opt in ("-d", "--debug"):
+               self.DEBUG_MODE = True
+       print 'Debug Mode: ', self.DEBUG_MODE
 
     def handleState(self):
         self._state = (self.state.handleInput())
@@ -72,16 +91,16 @@ class _Game():
         pass
 
 
-def _Initiate():
+def _Initiate(argv):
     pygame.init()
     SURFACE = pygame.display.set_mode(
         [util.SURFACE_WIDTH, util.SURFACE_HEIGHT], pygame.HWSURFACE |
         pygame.DOUBLEBUF)
     pygame.display.set_caption(util.SURFACE_CAPTION)
     pygame.display.set_icon(util.ICON)
-    game = _Game(SURFACE)
+    game = _Game(argv, SURFACE)
 
     pygame.quit()
 
 if __name__ == "__main__":
-    _Initiate()
+    _Initiate(sys.argv[1:])
